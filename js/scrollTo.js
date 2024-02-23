b01c3c56874cefbecd7388592ea354533046c17c
duangtan <duangta_n@silpakorn.edu>
2024-02-23 09:23:39 +0700

    first commit

---
+++ js/scrollTo.js
@@ -0,0 +1,34 @@
+function scrollToSunday() {
+    var target = document.querySelector(".banner_sunday_text");
+    target.scrollIntoView();
+}
+
+function scrollToMonday() {
+    var target = document.querySelector(".banner_monday_text");
+    target.scrollIntoView();
+}
+
+function scrollToTuesday() {
+    var target = document.querySelector(".banner_tuesday_text");
+    target.scrollIntoView();
+}
+
+function scrollToWednesday() {
+    var target = document.querySelector(".banner_wednesday_text");
+    target.scrollIntoView();
+}
+
+function scrollToThrusday() {
+    var target = document.querySelector(".banner_thrusday_text");
+    target.scrollIntoView();
+}
+
+function scrollToFriday() {
+    var target = document.querySelector(".banner_friday_text");
+    target.scrollIntoView();
+}
+
+function scrollToSaturday() {
+    var target = document.querySelector(".banner_saturday_text");
+    target.scrollIntoView();
+}
